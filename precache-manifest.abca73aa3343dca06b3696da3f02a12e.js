cf7c499ca5e8210098ea7a51acc1f6ed8e0bc8e7
Kash Nouroozi <knouroozi@gmail.com>
2019-09-21 18:24:13 -0400

    Updates

--- precache-manifest.abca73aa3343dca06b3696da3f02a12e.js
+++ precache-manifest.abca73aa3343dca06b3696da3f02a12e.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1c87f26509db78ed2d1a9b9a7661772c",
+    "revision": "ae10c120b8a6ba18ff995e92944954f1",
     "url": "/bf-visualizer/index.html"
   },
   {
-    "revision": "0995df1b10c356fa2e25",
+    "revision": "dcce956dbc444e9ec8c7",
     "url": "/bf-visualizer/static/css/main.734cf44f.chunk.css"
   },
   {
-    "revision": "da5ba82cd954c734c4df",
-    "url": "/bf-visualizer/static/js/2.bc05a658.chunk.js"
+    "revision": "27db0f59d5f52947043c",
+    "url": "/bf-visualizer/static/js/2.79751f44.chunk.js"
   },
   {
-    "revision": "0995df1b10c356fa2e25",
-    "url": "/bf-visualizer/static/js/main.7e623255.chunk.js"
+    "revision": "dcce956dbc444e9ec8c7",
+    "url": "/bf-visualizer/static/js/main.cb85e4e5.chunk.js"
   },
   {
     "revision": "4afd2eee85e782240527",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bf-visualizer/precache-manifest.afca8b331097179e9dae5e7245349b07.js"
+  "/bf-visualizer/precache-manifest.abca73aa3343dca06b3696da3f02a12e.js"
 );
 
 self.addEventListener('message', (event) => {
